e8ae9a3a04e02b3c094ba5e104d087d13a9fb442
Ester Martínez <ester.frontend@gmail.com>
2024-04-11 12:05:18 +0200

    Install morgan and cookie-parser. Add passport midldeware to config

--- config/index.js
+++ config/index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const passport = require('../passport')
 
 const config = (app) => {
+    app.set('trust proxy', 1)
+    app.use(morgan('dev'))
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
+    app.use(cookieParser());
+    passport(app)
 }
 
 module.exports = config
