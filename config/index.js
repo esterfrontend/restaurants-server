3053f317da2044f776a81a5a539d6b40f73c34f7
Ester Martínez <ester.frontend@gmail.com>
2024-04-14 19:56:50 +0200

    Cors

--- config/index.js
+++ config/index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const passport = require('../passport')
+const cors = require("cors")
+
+const FRONTEND_URL = process.env.FRONTEND_ORIGIN || "http://localhost:3001"
+
+const corsOptions = {
+    origin: [FRONTEND_URL],
+    credentials: true
+}
 
 const config = (app) => {
     app.set('trust proxy', 1)
+    app.use(cors(corsOptions))
     app.use(morgan('dev'))
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
